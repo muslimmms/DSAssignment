f468ad9efd56842bf495883f5401dfa45f685598
Ahmad Muslim Solehin Bin Mohd Amin <22004748@siswa.um.edu.my>
2024-01-01 13:14:16 +0800

    Merging Stack and Linked List System into Main System

--- Scheduler/SchedulingSystems.java
+++ Scheduler/SchedulingSystems.java
@@ -62,7 +62,9 @@ public class SchedulingSystems {
         long totalTurnaroundTime = 0; // Total turnaround time for all tasks
 
         // Print header for the table
-        System.out.printf("%-5s %-40s %-30s %-20s %-30s %-20s %-20s%n",
+        System.out.println();
+        System.out.println("Priority Queue Scheduler System");
+        System.out.printf("%-5s %-80s %-20s %-20s %-30s %-20s %-20s%n",
                 "Count",
                 "Task",
                 "Started Time",
@@ -78,7 +80,7 @@ public class SchedulingSystems {
             // Calculate and print the response and turnaround times for the current task
             long responseTime = currentTime / 1000; // Convert nanoseconds to microseconds
             long turnaroundTime = (currentTime + executionTime) / 1000; // Convert nanoseconds to microseconds
-            System.out.printf("%-5d %-40s %-30s %-20s %-30s %-20s %-20s%n",
+            System.out.printf("%-5s %-80s %-20s %-20s %-30s %-20s %-20s%n",
                     taskCount,
                     currentTask,
                     currentTime,
@@ -101,16 +103,16 @@ public class SchedulingSystems {
         long averageTurnaroundTime = totalTurnaroundTime / queueSystem.getSize();
         System.out.printf("Average Response Time: %-20s%n", averageResponseTime + " microseconds");
         System.out.printf("Average Turnaround Time: %-20s%n", averageTurnaroundTime + " microseconds");
-    }
-    
+    } 
     public void executeQueueSystemPriority() {
         int taskCount = 1; // Initialize the task count
         long currentTime = 0; // Keep track of the current time in nanoseconds
         long totalResponseTime = 0; // Total response time for all tasks
         long totalTurnaroundTime = 0; // Total turnaround time for all tasks
-
         // Print header for the table
-        System.out.printf("%-5s %-40s %-30s %-20s %-30s %-20s %-20s%n",
+        System.out.println();
+        System.out.println("Priority Queue Scheduler System");
+        System.out.printf("%-5s %-80s %-20s %-20s %-30s %-20s %-20s%n",
                 "Count",
                 "Task",
                 "Started Time",
@@ -129,7 +131,7 @@ public class SchedulingSystems {
             // Calculate and print the response and turnaround times for the current task
             long responseTime = currentTime / 1000; // Convert nanoseconds to microseconds
             long turnaroundTime = (currentTime + executionTime) / 1000; // Convert nanoseconds to microseconds
-            System.out.printf("%-5d %-40s %-30s %-20s %-30s %-20s %-20s%n",
+            System.out.printf("%-5s %-80s %-20s %-20s %-30s %-20s %-20s%n",
                     taskCount,
                     currentTask,
                     currentTime,
@@ -180,9 +182,6 @@ public class SchedulingSystems {
                     tempQueue.enqueue(currentTask);
                 }
             }
-                    // Print statements to check the sorting process
-            System.out.println("Selected Task: " + minTask);
-            System.out.println("Current Queue: " + tempQueue);
             // Enqueue the sorted task into the original queue
             queueSystem.enqueue(minTask);
         }
@@ -194,12 +193,96 @@ public class SchedulingSystems {
     }
     // Method to execute tasks using LinkedList scheduling system
     public void executeLinkedListSystem() {
-        // Implement the execution logic using LinkedList
-    }
+        int taskCount = 1; // Initialize the task count
+        long currentTime = 0; // Keep track of the current time in nanoseconds
+        long totalResponseTime = 0; // Total response time for all tasks
+        long totalTurnaroundTime = 0; // Total turnaround time for all tasks
+        
+        // Print header for the tables
+        System.out.println();
+        System.out.println("Linked List Scheduler System");
+        System.out.printf("%-5s %-80s %-20s %-20s %-30s %-20s %-20s%n",
+                "Count",
+                "Task",
+                "Started Time",
+                "Response Time",
+                "Completed Time",
+                "Turnaround Time",
+                "Execution Time");
+        
+        while(linkedListSystem.getSize() > 0){
+            Task currentTask = linkedListSystem.removeFirst();
+            long executionTime = Task.calculateExecutionTime(currentTask);
 
+            // Calculate and print the response and turnaround times for the current task
+            long responseTime = currentTime / 1000; // Convert nanoseconds to microseconds
+            long turnaroundTime = (currentTime + executionTime) / 1000; // Convert nanoseconds to microseconds
+            System.out.printf("%-5s %-80s %-20s %-20s %-30s %-20s %-20s%n",
+                    taskCount,
+                    currentTask,
+                    currentTime,
+                    responseTime + " microseconds",
+                    (currentTime + executionTime)+ " microseconds",
+                    turnaroundTime + " microseconds",
+                    executionTime + " microseconds");
+            
+            // Update total response and turnaround times
+            totalResponseTime += responseTime;
+            totalTurnaroundTime += turnaroundTime;
+
+            // Update the current time
+            currentTime += executionTime;
+            taskCount++;
+        }
+    }
     // Method to execute tasks using Stack scheduling system
     public void executeStackSystem() {
-        // Implement the execution logic using Stack (LIFO)
+        int taskCount = 1; // Initialize the task count
+        long currentTime = 0; // Keep track of the current time in nanoseconds
+        long totalResponseTime = 0; // Total response time for all tasks
+        long totalTurnaroundTime = 0; // Total turnaround time for all tasks 
+        //Print header for the titles
+        System.out.println();
+        System.out.println("Stack Scheduler System");
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%-5s %-80s %-20s %-20s %-30s %-20s %-20s%n",             
+            "Count",
+            "Task",
+            "Started Time",
+            "Response Time",
+            "Completed Time",
+            "Turnaround Time",
+            "Execution Time"));
+         System.out.print(sb);
+        
+         while(!stackSystem.isEmpty()){
+                Task current = stackSystem.pop();
+                long executionTime = Task.calculateExecutionTime(current);
+                // Calculate and print the response and turnaround times for the current task
+                long responseTime = currentTime / 1000; // Convert nanoseconds to microseconds
+                long turnaroundTime = (currentTime + executionTime) / 1000; // Convert nanoseconds to microseconds
+
+                StringBuilder output = new StringBuilder();
+                output.append(String.format("%-5d %-80s %-20s %-20s %-30s %-20s %-20s%n",
+                taskCount, current, currentTime, responseTime + " microseconds",
+                    (currentTime + executionTime) + " microseconds", turnaroundTime + " microseconds",
+                    executionTime + " microseconds"));
+
+                    System.out.print(output);
+
+                // Update total response and turnaround times
+                totalResponseTime += responseTime;
+                totalTurnaroundTime += turnaroundTime;
+
+                // Update the current time
+                currentTime += executionTime;
+                taskCount++;             
+         }
+        // Print average response and turnaround times
+        long avgResponseTime = totalResponseTime / taskCount;
+        long avgTurnaroundTime = totalTurnaroundTime / taskCount;
+        System.out.println("Average Response Time: " + avgResponseTime + " microseconds");
+        System.out.println("Average Turnaround Time: " + avgTurnaroundTime + " microseconds");   
     }
     
 }
